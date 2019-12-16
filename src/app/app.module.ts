0e55e490f2afc88602afa68e3ca580bf434984a3
umanglani <umanglani@gmail.com>
2019-12-16 15:23:42 +0800

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, FontAwesomeModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
