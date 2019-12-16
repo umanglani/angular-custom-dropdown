f4e5c29dca6e3a4adf84aa8e986fb1b15110c676
umanglani <umanglani@gmail.com>
2019-12-16 17:13:04 +0800

    3 files modified

--- src/app/dropdown/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown/dropdown.component.ts
@@ -8,14 +8,22 @@ import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 })
 export class DropdownComponent implements OnInit {
 
-  selectedText: string;
+  selectedItem: {id: number, text: string};
   items: Array<{id: number, text: string}>;
+  showItems: boolean;
 
   constructor() { }
 
   ngOnInit() {
     this.items = new Array<{id: number, text: string}>();
-    this.selectedText = "testing";
+    this.items.push({id: 1, text: 'On Schedule'});
+    this.items.push({id: 2, text: 'Behind Schedule'});
+    this.items.push({id: 3, text: 'On Hold'});
+    this.selectedItem = this.items[0];
+    this.showItems = false;
   }
 
+  toggleList() {
+    this.showItems = !this.showItems;
+  }
 }
