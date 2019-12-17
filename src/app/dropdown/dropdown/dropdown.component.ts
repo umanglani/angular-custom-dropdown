7708ed1cccfddfb1c5048cc35810fd061d2f9541
umanglani <umanglani@gmail.com>
2019-12-17 13:46:40 +0800

    3 files modified

--- src/app/dropdown/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown/dropdown.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -12,6 +12,13 @@ export class DropdownComponent implements OnInit {
   items: Array<{id: number, text: string}>;
   showItems: boolean;
 
+  @HostListener('document:click', ['$event'])
+  documentClick(event: any): void {
+    if (this.showItems) {
+      this.showItems = false;
+    }
+  }
+
   constructor() { }
 
   ngOnInit() {
@@ -19,11 +26,18 @@ export class DropdownComponent implements OnInit {
     this.items.push({id: 1, text: 'On Schedule'});
     this.items.push({id: 2, text: 'Behind Schedule'});
     this.items.push({id: 3, text: 'On Hold'});
+    this.items.push({id: 2, text: 'Behind Schedule'});
+    this.items.push({id: 3, text: 'On Hold'});
+    this.items.push({id: 2, text: 'Behind Schedule'});
+    this.items.push({id: 3, text: 'On Hold'});
+    this.items.push({id: 2, text: 'Behind Schedule'});
+    this.items.push({id: 3, text: 'On Hold'});
     this.selectedItem = this.items[0];
     this.showItems = false;
   }
 
-  toggleList() {
+  toggleList($event) {
+    $event.stopPropagation();
     this.showItems = !this.showItems;
   }
 }
