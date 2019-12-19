3473bad9ee6df96f475d5d379ccb78fd53a88d8f
umanglani <umanglani@gmail.com>
2019-12-19 11:19:25 +0800

    3 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 import { AppComponent } from './app.component';
 import { DropdownComponent } from './dropdown/dropdown/dropdown.component';
 import { TabsComponent } from './tabs/tabs/tabs.component';
+import { PersonComponent } from './person/person.component';
+import { ImgBigDirective, ImgMediumDirective, ImgSmallDirective } from './person/img-big.directive';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, FontAwesomeModule ],
-  declarations: [ AppComponent, DropdownComponent, TabsComponent ],
+  declarations: [ AppComponent, DropdownComponent, TabsComponent, PersonComponent, ImgBigDirective, ImgMediumDirective, ImgSmallDirective ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { 
---
+++ src/app/person/img-big.directive.ts
@@ -0,0 +1,52 @@
+import { Directive, ElementRef, OnInit, AfterViewInit, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appImgBig]'
+})
+export class ImgBigDirective implements OnInit, AfterViewInit {
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+  }
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    const container = this.el.nativeElement.querySelector('div');
+    this.renderer.addClass(container, 'img-big');
+  }
+
+}
+
+@Directive({
+  selector: '[appImgMedium]'
+})
+export class ImgMediumDirective implements OnInit, AfterViewInit {
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+  }
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    const container = this.el.nativeElement.querySelector('div');
+    this.renderer.addClass(container, 'img-medium');
+  }
+
+}
+
+@Directive({
+  selector: '[appImgSmall]'
+})
+export class ImgSmallDirective implements OnInit, AfterViewInit {
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {
+  }
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    const container = this.el.nativeElement.querySelector('div');
+    this.renderer.addClass(container, 'img-small');
+  }
+
+}
